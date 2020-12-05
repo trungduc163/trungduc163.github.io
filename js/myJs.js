67b4bf7b9a34c4c35a4db72973bab58fa1658b9a
khunglongbita <56436939+khunglongbita@users.noreply.github.com>
2020-12-05 20:51:52 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -96,7 +96,7 @@ $('#yes').click(function() {
     var audio = new Audio('sound/tick.mp3');
     audio.play();
     Swal.fire({
-        title: 'Nói cho tớ lí do chị thích em đi :vvvv',
+        title: 'Nói cho em lí do chị thích em đi :vvvv',
         html: true,
         width: 900,
         padding: '3em',
