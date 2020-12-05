fcb1ed72eb861f6be540121ce0a1b780a53c27a4
khunglongbita <56436939+khunglongbita@users.noreply.github.com>
2020-12-05 20:49:46 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -14,8 +14,8 @@ function firstQuestion(){
     
     $('.content').hide();
     Swal.fire({
-        title: 'He luu cậu!',
-        text: 'Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.',
+        title: 'He luu chị!',
+        text: 'Em có điều này muốn hỏi chị nhớ phải trả lời thật lòng nhaaa.',
         imageUrl: 'img/cuteCat.jpg',
         imageWidth: 300,
         imageHeight: 300,
@@ -73,7 +73,7 @@ $('#no').click(() => {
 // generate text in input
 function textGenerate() {
     var n = "";
-    var text = " Tại vì cậu ấm áp hehe :<<<<<<< ";
+    var text = " Tại vì chị ấm áp hehe :<<<<<<< ";
     var a = Array.from(text);
     var textVal = $('#txtReason').val() ? $('#txtReason').val() : "";
     var count = textVal.length;
@@ -96,7 +96,7 @@ $('#yes').click(function() {
     var audio = new Audio('sound/tick.mp3');
     audio.play();
     Swal.fire({
-        title: 'Nói cho tớ lí do cậu thích tớ đi :vvvv',
+        title: 'Nói cho tớ lí do chị thích em đi :vvvv',
         html: true,
         width: 900,
         padding: '3em',
@@ -121,8 +121,8 @@ $('#yes').click(function() {
                 width: 900,
                 confirmButtonText: 'Okiiiii lun <3',
                 background: '#fff url("img/iput-bg.jpg")',
-                title: 'Tớ biết mà ^^ Yêu cậu 300.000',
-                text: "Còn chờ gì nữa mà không inbox cho tớ đi nào hehe :))))",
+                title: 'Em biết mà ^^ Yêu chị 300.000',
+                text: "Còn chờ gì nữa mà không inbox cho em đi nào hehe :))))",
                 confirmButtonColor: '#83d0c9',
                 onClose: () => {
                       window.location ='https://www.facebook.com/metatron.8320/';
