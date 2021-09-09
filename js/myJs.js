afec932ea9176c1b080a8e2e1c52a53d7605ef01
khunglongbita <56436939+khunglongbita@users.noreply.github.com>
2021-09-09 21:48:56 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -114,7 +114,7 @@ $('#yes').click(function() {
         cancelButtonColor: '#d33',
         confirmButtonColor: '#fe8a71',
         cancelButtonColor: '#f6cd61',
-        confirmButtonText: 'Gửi cho em <3'
+        confirmButtonText: 'Gửi cho tớ <3'
     }).then((result) => {
         if (result.value) {
             Swal.fire({
