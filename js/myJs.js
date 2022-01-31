484b30e96763a3415fd8bf01e2c5bf97506a476e
khunglongbita <56436939+khunglongbita@users.noreply.github.com>
2022-01-31 21:04:43 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -125,7 +125,7 @@ $('#yes').click(function() {
                 text: "Còn chờ gì nữa mà không inbox cho tớ đi nào hehe :))))",
                 confirmButtonColor: '#83d0c9',
                 onClose: () => {
-                      window.location ='https://www.facebook.com/kzendo.8';
+                      window.location ='https://www.facebook.com/kzendo.8/';
                   }
             })
         }
